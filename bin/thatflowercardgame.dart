7acb234350ec820baf51d0e1f0b90c72f95a548f
Sergio Alejandro Galvan Gastelum <kingbluedude@gmail.com>
2023-03-28 07:22:56 -0700

    made the deck builder

--- bin/thatflowercardgame.dart
+++ bin/thatflowercardgame.dart
@@ -1,5 +1,5 @@
 import 'package:thatflowercardgame/thatflowercardgame.dart' as thatflowercardgame;
 
 void main(List<String> arguments) {
-  print('Hello world: ${thatflowercardgame.calculate()}!');
+ 
 }
--- test/thatflowercardgame_test.dart
+++ test/thatflowercardgame_test.dart
@@ -2,7 +2,32 @@ import 'package:thatflowercardgame/thatflowercardgame.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
+  test('should return white lily', () {
+    
   });
+}
+
+class Deck {
+  List<Card> cards = [];
+  
+  Deck(){
+  var flowers = ['Lily'];
+  var colors = ['White'];
+
+  for (var flower in flowers){
+    for  (var color in colors){
+      var card = new Card(flower, color);
+      cards.add(card);
+    }
+
+  }
+  }
+
+
+}
+
+class Card{
+  String flower;
+  String color;
+  Card(this.flower, this.color);
 }
